44bd11bba838a46f0d1c66c547001addfcdc6984
Eoin Landy <el111@yahoo.com>
2017-10-02 00:13:46 +0100

    no message

--- example/get_ios_dependencies.sh
+++ example/get_ios_dependencies.sh
@@ -4,9 +4,9 @@ wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/1.2.0/ios_depende
 unzip -u -o ios_dependencies.zip
 rm ios_dependencies.zip
 
-wget https://github.com/tuarua/GoogleMapsANE/releases/download/0.0.7/ios_dependencies.zip
+wget https://github.com/tuarua/GoogleMapsANE/releases/download/0.0.8/ios_dependencies.zip
 unzip -u -o ios_dependencies.zip
 rm ios_dependencies.zip
 
-wget -O ../native_extension/ane/GoogleMapsANE.ane https://github.com/tuarua/Google-Maps-ANE/releases/download/0.0.7/GoogleMapsANE.ane?raw=true
+wget -O ../native_extension/ane/GoogleMapsANE.ane https://github.com/tuarua/Google-Maps-ANE/releases/download/0.0.8/GoogleMapsANE.ane?raw=true
 
