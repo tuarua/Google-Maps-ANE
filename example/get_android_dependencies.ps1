9dd5a55517071f62132af67f1c780e22d83220fe
Eoin Landy <el111@yahoo.com>
2018-02-26 21:34:02 +0000

    github downloads needs TLS1.2

--- example/get_android_dependencies.ps1
+++ example/get_android_dependencies.ps1
@@ -1,4 +1,5 @@
 $currentDir = (Get-Item -Path ".\" -Verbose).FullName
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri https://github.com/tuarua/Google-Maps-ANE/releases/download/1.3.0/GoogleMapsANE.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\GoogleMapsANE.ane"
 Invoke-WebRequest -OutFile "$currentDir\android_dependencies\com.tuarua.frekotlin.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/kotlin/com.tuarua.frekotlin.ane?raw=true
 Invoke-WebRequest -OutFile "$currentDir\android_dependencies\org.greenrobot.eventbus-3.0.0.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/misc/org.greenrobot.eventbus-3.0.0.ane?raw=true
