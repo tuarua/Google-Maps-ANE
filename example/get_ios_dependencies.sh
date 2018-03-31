9daf1fc9620e0a151e2a5ce9766a76bf6bf42962
Eoin Landy <el111@yahoo.com>
2018-03-31 15:24:25 +0100

    1.5.0

--- example/get_ios_dependencies.sh
+++ example/get_ios_dependencies.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/2.3.0/ios_dependencies.zip
+rm -r ios_dependencies/device
+rm -r ios_dependencies/simulator
+
+wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/2.4.0/ios_dependencies.zip
 unzip -u -o ios_dependencies.zip
 rm ios_dependencies.zip
 
-wget https://github.com/tuarua/Google-Maps-ANE/releases/download/1.4.0/ios_dependencies.zip
+wget https://github.com/tuarua/Google-Maps-ANE/releases/download/1.5.0/ios_dependencies.zip
 unzip -u -o ios_dependencies.zip
 rm ios_dependencies.zip
 
-wget -O ../native_extension/ane/GoogleMapsANE.ane https://github.com/tuarua/Google-Maps-ANE/releases/download/1.4.0/GoogleMapsANE.ane?raw=true
+wget -O ../native_extension/ane/GoogleMapsANE.ane https://github.com/tuarua/Google-Maps-ANE/releases/download/1.5.0/GoogleMapsANE.ane?raw=true
 
